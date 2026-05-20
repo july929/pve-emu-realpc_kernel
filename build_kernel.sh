6755f36bf3b1e68731e7aca13fb50b3a62380086
July's <37677092+july929@users.noreply.github.com>
2026-05-20 13:15:53 +0800

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,7 +6,7 @@ ls
 df -h
 git clone git://git.proxmox.com/git/pve-kernel.git
 cd pve-kernel
-#git reset --hard 91ca55897a7e9a1451833216479098b05e6bc0f6 # bump version to 6.14.8-2
+git reset --hard 99f0035653fbaa69760fd49d373a8e74c245bb80 # bump version to 6.17.13-2
 apt install devscripts -y
 mk-build-deps --install
 git submodule update --init --recursive --force #强制更新所有子模块
